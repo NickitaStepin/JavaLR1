cc70a391dcdc5b93a467ba2979ec1a4c797cd60a
Student <student@example.com>
2025-10-07 15:29:05 +0300

    РєРѕРјРёС‚ 1

--- Passport.java
+++ Passport.java
@@ -6,15 +6,15 @@ public class Passport {
     public static void main(String[] args) {
         // Вывод ФИО
         System.out.println("=== ПАСПОРТ СТУДЕНТА ===");
-        System.out.println("ФИО: Иванов Иван Иванович");
-        System.out.println("Группа: ИТ-21-1");
+        System.out.println("ФИО: Степин Никита Дмитриевич");
+        System.out.println("Группа: ИБ-23-1");
         System.out.println();
         
         // Краткая информация о себе
         System.out.println("=== КРАТКАЯ ИНФОРМАЦИЯ ===");
-        System.out.println("Специальность: Информационные технологии");
+        System.out.println("Специальность: Информационная безопасность");
         System.out.println("Курс: 2");
-        System.out.println("Университет: Технический университет");
+        System.out.println("Университет: Приазовский Государственный Технический университет");
         System.out.println("Хобби: Программирование, чтение книг");
         System.out.println("Цель: Стать профессиональным разработчиком");
         System.out.println();
